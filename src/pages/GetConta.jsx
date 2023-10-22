b5a7d4b5cca99987fa5bef6797494371ee3d8fa0
Davi <davilinfo@gmail.com>
2023-10-22 07:59:19 -0300

    react frontend to connect with azure AD

---
+++ src/pages/GetConta.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import { MsalAuthenticationTemplate } from '@azure/msal-react';
+import { InteractionType } from '@azure/msal-browser';
+import { useMsal } from "@azure/msal-react";
+
+import { loginRequest, protectedResources } from "../authConfig";
+import useFetchWithMsal from '../hooks/useFetchWithMsal';
+
+const GetContaContent = () => {
+    const { error, execute } = useFetchWithMsal({
+        scopes: protectedResources.apiTodoList.scopes.read,
+    });
+
+    const { instance } = useMsal();
+    const activeAccount = instance.getActiveAccount();    
+    const email = activeAccount.idTokenClaims.preferred_username;
+
+    let getContaData = null;
+
+    useEffect(() => {
+        if (!getContaData) {
+            execute("GET", protectedResources.apiTodoList.endpoint + `/GetConta?Email=${email}`).then((response) => {                
+                getContaData = response.length > 0 ? response[0].id : null;
+            }).catch((er)=>{
+                console.log("erro fetch", er.message);
+            });
+        }
+    }, [execute, getContaData])    
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return <> id conta {getContaData} </>;
+};
+
+/**
+ * The `MsalAuthenticationTemplate` component will render its children if a user is authenticated
+ * or attempt to sign a user in. Just provide it with the interaction type you would like to use
+ * (redirect or popup) and optionally a request object to be passed to the login API, a component to display while
+ * authentication is in progress or a component to display if an error occurs. For more, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+ */
+export const GetConta = () => {
+    const authRequest = {
+        ...loginRequest,
+    };
+
+    return (
+        <MsalAuthenticationTemplate 
+            interactionType={InteractionType.Redirect} 
+            authenticationRequest={authRequest}
+        >
+            <GetContaContent />
+        </MsalAuthenticationTemplate>
+    );
+};
